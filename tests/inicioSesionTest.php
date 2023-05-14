2100d85cf4f437a2655ff62b915a16d59515ac45
luisferleerod <luisferleerod@gmail.com>
2023-05-14 09:32:07 -0500

    Primera prueba jenkins automatizado

---
+++ tests/inicioSesionTest.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__.'/../conexion/dbh.inc.php';
+
+use PHPUnit\Framework\TestCase;
+
+class InicioSesionTest extends TestCase {
+    // Métodos de prueba aquí
+    public function testAssertEquals() {
+        $mensaje = "Hola Mundo";
+        $this->assertEquals("Hola Mundo", $mensaje);
+    }
+
+    public function testAssertTrue() {
+        $esVerdadero = true;
+        $this->assertTrue($esVerdadero);
+    }
+
+    public function testAssertFalse() {
+        $esFalso = false;
+        $this->assertFalse($esFalso);
+    }
+}
