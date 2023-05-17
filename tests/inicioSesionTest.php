209d20e7f9f1dc713e34538309d733bb195dc008
luisferleerod <102424560+luisferleerod@users.noreply.github.com>
2023-05-16 21:09:14 -0500

    Vuelvo a colocar el documento de referencia
    programacion de pruebas basadas en un hello world

--- tests/inicioSesionTest.php
+++ tests/inicioSesionTest.php
@@ -1,34 +1,21 @@
+<?php
+
 use PHPUnit\Framework\TestCase;
 
-class InicioSesionTest extends TestCase
-{
-    public function testInicioSesionExitoso()
-    {
-        // Simula los datos de inicio de sesión correctos
-        $_POST["username"] = "jjosegomez";
-        $_POST["password"] = "12345";
-
-        // Incluye el archivo de inicio de sesión para acceder a las variables y funciones
-        include("inicioSesion.php");
-
-        // Comprueba si se redirecciona correctamente después del inicio de sesión exitoso
-        $this->assertEquals("../interfaz/principal.php", $this->getActualOutput());
-
-        // Comprueba si se estableció la variable de sesión correctamente
-        $this->assertEquals("usuario_valido", $_SESSION['username']);
+class InicioSesionTest extends TestCase {
+    // Métodos de prueba aquí
+    public function testAssertEquals() {
+        $mensaje = "Hola Mundo";
+        $this->assertEquals("Hola Mundo", $mensaje);
     }
 
-    public function testInicioSesionFallido()
-    {
-        // Simula los datos de inicio de sesión incorrectos
-        $_POST["username"] = "jjosegomez1";
-        $_POST["password"] = "123456";
+    public function testAssertTrue() {
+        $esVerdadero = true;
+        $this->assertTrue($esVerdadero);
+    }
 
-        // Incluye el archivo de inicio de sesión para acceder a las variables y funciones
-        include("inicioSesion.php");
-
-        // Comprueba si se muestra el mensaje de error
-        $this->expectOutputString('<script language="javascript">alert("Usuario o clave incorrectos");window.location.href="../interfaz/index.html"</script>');
-        $this->assertEquals(0, mysqli_num_rows($query));
+    public function testAssertFalse() {
+        $esFalso = false;
+        $this->assertFalse($esFalso);
     }
 }
