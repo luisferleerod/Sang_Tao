8739cf4f68c6b0200b6d128ac660fbd41deca162
luisferleerod <102424560+luisferleerod@users.noreply.github.com>
2023-05-16 21:46:28 -0500

    Prueba 7 Inicio Sesion test
    Intento 7 prueba automatizada inicio Sesion

--- tests/inicioSesionTest.php
+++ tests/inicioSesionTest.php
@@ -1,9 +1,13 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+include ("../conexion/dbh.inc.php");
+
 
 class InicioSesionTest extends TestCase {
     private $db;
+    $username = 'jjosegomez';
+    $password = '12345';
 
     protected function setUp(): void {
         $this->db = new mysqli("192.168.77.45", "jjosegomez", "luchopelucho", "casti");
@@ -13,29 +17,26 @@ class InicioSesionTest extends TestCase {
     }
 
     public function testValidLogin() {
-        $username = 'jjosegomez';
-        $password = '12345';
+        $esVerdadero;
+        
+        if(isset($_POST["entrar"])){
+            
+            $query = mysqli_query($con,"SELECT * FROM usuario WHERE usuario = '$username' AND clave = '$password'");
+            $nr = mysqli_num_rows($query);
 
-        $query = "SELECT * FROM usuario WHERE usuario = '$username' AND clave = '$password'";
-        $result = $this->db->query($query);
-        $nr = $result->num_rows;
+            if($nr==1){
+                $esVerdadero = true;
+                $this->assertTrue($esVerdadero);
+                
+                die();
+                
+            } else {
+                $esVerdadero = false;
+                $this->assertTrue($esVerdadero);
+            }
+        }
 
-        $this->assertEquals(1, $nr);
     }
 
-    public function testInvalidLogin() {
-        $username = 'jjosegomez';
-        $password = '123456';
-
-        $query = "SELECT * FROM usuario WHERE usuario = '$username' AND clave = '$password'";
-        $result = $this->db->query($query);
-        $nr = $result->num_rows;
-
-        $this->assertEquals(0, $nr);
-    }
-
-    protected function tearDown(): void {
-        $this->db->close();
-    }
 }
 ?>
