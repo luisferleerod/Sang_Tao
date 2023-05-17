5e9dbe74d8c49ab308936b771c1e99ab58696f97
luisferleerod <102424560+luisferleerod@users.noreply.github.com>
2023-05-16 21:12:22 -0500

    Prueba 4 InicioSesion test
    Intento 4 de prueba de automatizacion

--- tests/inicioSesionTest.php
+++ tests/inicioSesionTest.php
@@ -5,17 +5,30 @@ use PHPUnit\Framework\TestCase;
 class InicioSesionTest extends TestCase {
     // Métodos de prueba aquí
     public function testAssertEquals() {
-        $mensaje = "Hola Mundo";
-        $this->assertEquals("Hola Mundo", $mensaje);
-    }
+         // Simula los datos de inicio de sesión correctos
+        $_POST["username"] = "jjosegomez";
+        $_POST["password"] = "12345";
 
-    public function testAssertTrue() {
-        $esVerdadero = true;
-        $this->assertTrue($esVerdadero);
+        // Incluye el archivo de inicio de sesión para acceder a las variables y funciones
+        include("inicioSesion.php");
+
+        // Comprueba si se redirecciona correctamente después del inicio de sesión exitoso
+        $this->assertEquals("../interfaz/principal.php", $this->getActualOutput());
+
+        // Comprueba si se estableció la variable de sesión correctamente
+        $this->assertEquals("usuario_valido", $_SESSION['username']);
     }
 
     public function testAssertFalse() {
-        $esFalso = false;
-        $this->assertFalse($esFalso);
+        // Simula los datos de inicio de sesión incorrectos
+        $_POST["username"] = "jjosegomez1";
+        $_POST["password"] = "123456";
+
+        // Incluye el archivo de inicio de sesión para acceder a las variables y funciones
+        include("inicioSesion.php");
+
+        // Comprueba si se muestra el mensaje de error
+        $this->expectOutputString('<script language="javascript">alert("Usuario o clave incorrectos");window.location.href="../interfaz/index.html"</script>');
+        $this->assertEquals(0, mysqli_num_rows($query));
     }
 }
