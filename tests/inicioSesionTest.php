d87aeb22fbd8919fec937b53f3f5918bc964fa1d
luisferleerod <102424560+luisferleerod@users.noreply.github.com>
2023-05-16 19:02:01 -0500

    Primera prueba de validacion de usuario test
    Primer intento del codigo de validación de inicio de sesion

--- tests/inicioSesionTest.php
+++ tests/inicioSesionTest.php
@@ -2,20 +2,70 @@
 
 use PHPUnit\Framework\TestCase;
 
-class InicioSesionTest extends TestCase {
-    // Métodos de prueba aquí
-    public function testAssertEquals() {
-        $mensaje = "Hola Mundo1";
-        $this->assertEquals("Hola Mundo", $mensaje);
+class LoginTest extends TestCase
+{
+    public function testValidLogin()
+    {
+        // Datos de inicio de sesión válidos
+        $username = "jjosegomez";
+        $password = "luchopelucho";
+        
+        // Realizar la solicitud POST al archivo de inicio de sesión
+        $postData = [
+            'username' => $username,
+            'password' => $password,
+            'entrar' => true
+        ];
+        
+        $response = $this->postRequest('ruta_al_archivo/inicioSesion.php', $postData);
+        
+        // Verificar que se redireccionó a la página principal
+        $this->assertEquals('Location: ../interfaz/principal.php', $response['headers'][0]);
+        
+        // Verificar que la sesión se inició correctamente
+        $this->assertTrue(session_id() !== '', 'La sesión no se inició correctamente');
+        $this->assertEquals($username, $_SESSION['username'], 'El nombre de usuario en la sesión no coincide');
     }
-
-    public function testAssertTrue() {
-        $esVerdadero = false;
-        $this->assertTrue($esVerdadero);
+    
+    public function testInvalidLogin()
+    {
+        // Datos de inicio de sesión inválidos
+        $username = "usuario_invalido";
+        $password = "clave_invalida";
+        
+        // Realizar la solicitud POST al archivo de inicio de sesión
+        $postData = [
+            'username' => $username,
+            'password' => $password,
+            'entrar' => true
+        ];
+        
+        $response = $this->postRequest('ruta_al_archivo/inicioSesion.php', $postData);
+        
+        // Verificar que se mostró un mensaje de error y se redireccionó al formulario de inicio de sesión
+        $this->assertStringContainsString('Usuario o clave incorrectos', $response['content']);
+        $this->assertEquals('Location: ../interfaz/index.html', $response['headers'][0]);
+        
+        // Verificar que la sesión no se inició
+        $this->assertTrue(session_id() === '', 'Se inició una sesión en un inicio de sesión inválido');
+        $this->assertArrayNotHasKey('username', $_SESSION, 'Se guardó un nombre de usuario en la sesión');
     }
-
-    public function testAssertFalse() {
-        $esFalso = true;
-        $this->assertFalse($esFalso);
+    
+    // Función auxiliar para realizar una solicitud POST y obtener la respuesta
+    private function postRequest($url, $data)
+    {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        $headers = curl_getinfo($ch);
+        curl_close($ch);
+        
+        return [
+            'headers' => $headers,
+            'content' => $response
+        ];
     }
 }
+
