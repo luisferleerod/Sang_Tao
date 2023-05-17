d1692bde3f8eb3dffda2182023c9d690eac6aab2
luisferleerod <102424560+luisferleerod@users.noreply.github.com>
2023-05-16 19:21:16 -0500

    Prueba 3 de test de inicio de sesion
    Intento 3 de test para validacion de inicio de sesion

--- tests/inicioSesionTest.php
+++ tests/inicioSesionTest.php
@@ -1,73 +1,34 @@
-<?php
-
 use PHPUnit\Framework\TestCase;
 
-class LoginTest extends TestCase
+class InicioSesionTest extends TestCase
 {
-    public function testValidLogin()
+    public function testInicioSesionExitoso()
     {
-        // Datos de inicio de sesión válidos
-        $username = "jjosegomez";
-        $password = "luchopelucho";
-        
-        // Crear una instancia de la clase PDO para la conexión a la base de datos
-        $dbhost = "192.168.77.45";
-        $dbuser = "jjosegomez";
-        $dbpass = "luchopelucho";
-        $dbname = "casti";
-        
-        $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-        
-        // Preparar la consulta SQL para buscar el usuario
-        $stmt = $dbh->prepare("SELECT * FROM usuario WHERE usuario = :username AND clave = :password");
-        $stmt->bindParam(':username', $username);
-        $stmt->bindParam(':password', $password);
-        
-        // Ejecutar la consulta
-        $stmt->execute();
-        
-        // Verificar que se encontró un usuario válido
-        $this->assertEquals(1, $stmt->rowCount(), 'No se encontró un usuario válido');
-        
-        // Verificar que se redireccionó a la página principal
-        $this->assertStringContainsString('Location: ../interfaz/principal.php', implode(' ', $stmt->errorInfo()));
-        
-        // Verificar que la sesión se inició correctamente
-        $this->assertNotEmpty(session_id(), 'La sesión no se inició correctamente');
-        $this->assertEquals($username, $_SESSION['username'], 'El nombre de usuario en la sesión no coincide');
+        // Simula los datos de inicio de sesión correctos
+        $_POST["username"] = "jjosegomez";
+        $_POST["password"] = "12345";
+
+        // Incluye el archivo de inicio de sesión para acceder a las variables y funciones
+        include("inicioSesion.php");
+
+        // Comprueba si se redirecciona correctamente después del inicio de sesión exitoso
+        $this->assertEquals("../interfaz/principal.php", $this->getActualOutput());
+
+        // Comprueba si se estableció la variable de sesión correctamente
+        $this->assertEquals("usuario_valido", $_SESSION['username']);
     }
-    
-    public function testInvalidLogin()
+
+    public function testInicioSesionFallido()
     {
-        // Datos de inicio de sesión inválidos
-        $username = "usuario_invalido";
-        $password = "clave_invalida";
-        
-        // Crear una instancia de la clase PDO para la conexión a la base de datos
-        $dbhost = "192.168.77.45";
-        $dbuser = "jjosegomez";
-        $dbpass = "luchopelucho";
-        $dbname = "casti";
-        
-        $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-        
-        // Preparar la consulta SQL para buscar el usuario
-        $stmt = $dbh->prepare("SELECT * FROM usuario WHERE usuario = :username AND clave = :password");
-        $stmt->bindParam(':username', $username);
-        $stmt->bindParam(':password', $password);
-        
-        // Ejecutar la consulta
-        $stmt->execute();
-        
-        // Verificar que no se encontró un usuario válido
-        $this->assertEquals(0, $stmt->rowCount(), 'Se encontró un usuario válido en un inicio de sesión inválido');
-        
-        // Verificar que se mostró un mensaje de error y se redireccionó al formulario de inicio de sesión
-        $this->assertStringContainsString('Usuario o clave incorrectos', implode(' ', $stmt->errorInfo()));
-        $this->assertStringContainsString('Location: ../interfaz/index.html', implode(' ', $stmt->errorInfo()));
-        
-        // Verificar que la sesión no se inició
-        $this->assertEmpty(session_id(), 'Se inició una sesión en un inicio de sesión inválido');
-        $this
+        // Simula los datos de inicio de sesión incorrectos
+        $_POST["username"] = "jjosegomez1";
+        $_POST["password"] = "123456";
 
+        // Incluye el archivo de inicio de sesión para acceder a las variables y funciones
+        include("inicioSesion.php");
 
+        // Comprueba si se muestra el mensaje de error
+        $this->expectOutputString('<script language="javascript">alert("Usuario o clave incorrectos");window.location.href="../interfaz/index.html"</script>');
+        $this->assertEquals(0, mysqli_num_rows($query));
+    }
+}
